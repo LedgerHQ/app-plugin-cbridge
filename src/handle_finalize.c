aaf0ce4e14ad1c94d42417911b0486fb1fd6a210
Alexandre Paillier <alexandre.paillier@ledger.fr>
2023-10-16 11:19:15 +0200

    Updated to use the latest plugin SDK

--- src/handle_finalize.c
+++ src/handle_finalize.c
@@ -1,8 +1,6 @@
 #include "cbridge_plugin.h"
 
-void handle_finalize(void *parameters) {
-
-  ethPluginFinalize_t *msg = (ethPluginFinalize_t *)parameters;
+void handle_finalize(ethPluginFinalize_t *msg) {
   context_t *context = (context_t *)msg->pluginContext;
 
   msg->uiType = ETH_UI_TYPE_GENERIC;
--- src/handle_query_contract_ui.c
+++ src/handle_query_contract_ui.c
@@ -1,4 +1,5 @@
 #include "cbridge_plugin.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,7 +8,7 @@ void print_chain_id(uint32_t chainId, char *destination, size_t max_length) {
 }
 
 // Set UI for the "Send" screen.
-static void set_send_ui(ethQueryContractUI_t *msg, const context_t *context) {
+static bool set_send_ui(ethQueryContractUI_t *msg, const context_t *context) {
   strlcpy(msg->title, "Send", msg->titleLength);
 
   uint8_t decimals = context->decimals;
@@ -20,11 +21,11 @@ static void set_send_ui(ethQueryContractUI_t *msg, const context_t *context) {
     ticker = msg->network_ticker;
   }
 
-  amountToString(context->amount_in, sizeof(context->amount_in), decimals,
-                 ticker, msg->msg, msg->msgLength);
+  return amountToString(context->amount_in, sizeof(context->amount_in),
+                        decimals, ticker, msg->msg, msg->msgLength);
 }
 
-static void set_send_native_ui(ethQueryContractUI_t *msg,
+static bool set_send_native_ui(ethQueryContractUI_t *msg,
                                const context_t *context) {
   strlcpy(msg->title, "Send", msg->titleLength);
 
@@ -33,21 +34,22 @@ static void set_send_native_ui(ethQueryContractUI_t *msg,
 
   // Converts the uint256 number located in `eth_amount` to its string
   // representation and copies this to `msg->msg`.
-  amountToString(eth_amount, eth_amount_size, WEI_TO_ETHER, "ETH", msg->msg,
-                 msg->msgLength);
+  return amountToString(eth_amount, eth_amount_size, WEI_TO_ETHER, "ETH",
+                        msg->msg, msg->msgLength);
 }
 
 // Set UI for "Receive" screen.
-static void set_receive_ui(ethQueryContractUI_t *msg,
+static bool set_receive_ui(ethQueryContractUI_t *msg,
                            const context_t *context) {
   strlcpy(msg->title, "Destination Chain.", msg->titleLength);
 
   print_chain_id(context->dst_chain_id, msg->msg, msg->msgLength);
+  return true;
 }
 
-void handle_query_contract_ui(void *parameters) {
-  ethQueryContractUI_t *msg = (ethQueryContractUI_t *)parameters;
+void handle_query_contract_ui(ethQueryContractUI_t *msg) {
   context_t *context = (context_t *)msg->pluginContext;
+  bool ret = false;
 
   // msg->title is the upper line displayed on the device.
   // msg->msg is the lower line displayed on the device.
@@ -56,59 +58,52 @@ void handle_query_contract_ui(void *parameters) {
   memset(msg->title, 0, msg->titleLength);
   memset(msg->msg, 0, msg->msgLength);
 
-  msg->result = ETH_PLUGIN_RESULT_OK;
-
   // Adapt the cases for the screens you'd like to display.
 
   if (context->selectorIndex == POOL_BASED_SEND_ERC20) {
     switch (msg->screenIndex) {
     case 0:
-      set_send_ui(msg, context);
+      ret = set_send_ui(msg, context);
       break;
     case 1:
-      set_receive_ui(msg, context);
+      ret = set_receive_ui(msg, context);
       break;
     // Keep this
     default:
       PRINTF("Received an invalid screenIndex\n");
-      msg->result = ETH_PLUGIN_RESULT_ERROR;
-      return;
     }
   } else if (context->selectorIndex == POOL_BASED_SEND_NATIVE) {
     switch (msg->screenIndex) {
     case 0:
-      set_send_native_ui(msg, context);
+      ret = set_send_native_ui(msg, context);
       break;
     case 1:
-      set_receive_ui(msg, context);
+      ret = set_receive_ui(msg, context);
       break;
     // Keep this
     default:
       PRINTF("Received an invalid screenIndex\n");
-      msg->result = ETH_PLUGIN_RESULT_ERROR;
-      return;
     }
   } else if (context->selectorIndex == PEGGED_TOKEN_DEPOSIT_MINT) {
     switch (msg->screenIndex) {
     case 0:
-      set_send_ui(msg, context);
+      ret = set_send_ui(msg, context);
       break;
     case 1:
-      set_receive_ui(msg, context);
+      ret = set_receive_ui(msg, context);
       break;
     // Keep this
     default:
-      msg->result = ETH_PLUGIN_RESULT_ERROR;
-      return;
+      break;
     }
   } else if (context->selectorIndex == PEGGED_TOKEN_BURN_WITHDRAW) {
     switch (msg->screenIndex) {
     case 0:
-      set_send_ui(msg, context);
+      ret = set_send_ui(msg, context);
       break;
     default:
-      msg->result = ETH_PLUGIN_RESULT_ERROR;
       break;
     }
   }
+  msg->result = ret ? ETH_PLUGIN_RESULT_OK : ETH_PLUGIN_RESULT_ERROR;
 }
